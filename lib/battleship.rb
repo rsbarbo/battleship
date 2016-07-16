48f2cefe16a22bd0dea8604ef83d9acc57a70c8d
Raphael Barbo <rsbarbo@gmail.com>
2016-07-16 12:44:18 -0600

    added some tests to battleship

--- lib/battleship.rb
+++ lib/battleship.rb
@@ -2,5 +2,17 @@
 
 class Battleship
 
+  
+
+
+  def welcome_message
+    p "Welcome to BATTLESHIP" && "Would you like to (p)lay, read the (i)nstructions, or (q)uit?"
+  end
+
+  def game_welcome
+    welcome_message
+    user_input = gets.chomp.to_s.downcase
+  end
+
 
 end
--- test/battleship_test.rb
+++ test/battleship_test.rb
@@ -1,6 +1,24 @@
 require "./test_helper"
+require "./lib/battleship"
 
 
 class BattleshipTest < Minitest::Test
+
+  def test_class_battleship_is_valid?
+    assert(Battleship)
+  end
+
+  def test_welcome_message
+    battleship = Battleship.new
+    wlcm_msg = "Welcome to BATTLESHIP" && "Would you like to (p)lay, read the (i)nstructions, or (q)uit?"
+    assert_equal wlcm_msg, battleship.welcome_message
+  end
+
+    describe "Test Welcoming users" do
+      before { def user_input; "p" || "play" end}
+      it "expected_input" do
+        user_input.must_equal "p" || "play"
+      end
+    end
 
 end
