f26291db4dc1ed0c4d2aeb5f951796e1f75c556a
Raphael Barbo <rsbarbo@gmail.com>
2016-07-21 08:15:34 -0600

    one more update to the test

---
+++ test/player_board_test.rb
@@ -0,0 +1,22 @@
+require_relative "test_helper"
+require "./lib/player_board"
+
+class PlayerBoardTest < Minitest::Test
+
+  def test_moves_class_exits
+    assert PlayerBoard
+  end
+
+  def test_translate_ship_coords
+    assert_equal [[0, 0], [0, 1]], PlayerBoard.new.grab_coords("a1 a2")
+  end
+
+  def test_insert_ship_with_coordinates
+    assert_equal [[0, 0], [0, 1]], PlayerBoard.new.insert_ship([[0, 0], [0, 1]])
+  end
+
+  def test_place_ship
+    assert_equal :Ship, PlayerBoard.new.place_ship(0,1)
+  end
+
+end
